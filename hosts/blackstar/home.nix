dd3af17e860b458651255531dc549760c4475381
Drew DeVore <drew@devorcula.com>
2026-09-24 14:39:33 -0600

    remove some obsolete stuff

--- hosts/blackstar/home.nix
+++ hosts/blackstar/home.nix
@@ -26,16 +26,9 @@
         hdr = "auto";
         sdr_white = 400;
         vrr = "always";
-        # scale = 1.25;
       };
       "HDMI-A-1" = {
         mode = "1920x1080@60";
-        # Right-aligned with DP-1's right edge (4096) and below it (1152),
-        # in logical coordinates at scale 1.25.
-        # position = [
-        #   2560
-        #   1152
-        # ];
         position = [
           3150
           1440
@@ -43,7 +36,6 @@
         hdr = "auto";
         sdr_white = 400;
         vrr = "disabled";
-        # scale = 1.25;
       };
     };
 
